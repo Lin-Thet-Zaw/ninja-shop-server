9232aeaff9147613f135b3f8513d48ea84fdc837
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2025-01-22 18:22:08 +0630

    Change appConfig

--- src/main/java/com/ninjashop/ninjashop/config/AppConfig.java
+++ src/main/java/com/ninjashop/ninjashop/config/AppConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -20,12 +22,8 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeHttpRequests(Authorize -> Authorize
-                        // Allow login and signup without authentication
-                        .requestMatchers("/auth/signin", "/auth/signup").permitAll()
-                        .requestMatchers("/api/cart/**").authenticated()
+                .authorizeHttpRequests(Authorize ->Authorize
                         .requestMatchers("/api/admin/**").authenticated()
-                        .requestMatchers("/api/orders/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
                 .csrf().disable()
@@ -42,7 +40,7 @@ public class AppConfig {
                         cfg.setAllowCredentials(true);
                         cfg.setAllowedHeaders(Collections.singletonList("*"));
                         cfg.setExposedHeaders(Arrays.asList("Authorization"));
-                        cfg.setMaxAge(3600L);
+                        cfg.setMaxAge(3600L); // Corrected here
                         return cfg;
                     }
                 }).and().httpBasic().and().formLogin();
