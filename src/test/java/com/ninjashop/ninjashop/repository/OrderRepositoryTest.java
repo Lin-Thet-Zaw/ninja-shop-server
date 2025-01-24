79403f32eb1a7644ffdcff5ebf65f8b1fd77b6cf
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2025-01-24 21:16:46 +0630

    change  application-prod.properties for prod

---
+++ src/test/java/com/ninjashop/ninjashop/repository/OrderRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.ninjashop.ninjashop.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @Test
+    void getUsersOrders() {
+    }
+
+    @Test
+    void findByOrderId() {
+    }
+}
